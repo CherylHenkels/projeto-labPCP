61fbbf88129eed7d1175808d7e437f7d50c3cb5a
CherylHenkels <cherylhenkelss@gmail.com>
2024-04-13 15:35:49 -0300

    Setup inicial do sistema

---
+++ projeto-labPCP/src/main/java/br/fullstack/education/projetolabpcp/ProjetoLabPcpApplication.java
@@ -0,0 +1,13 @@
+package br.fullstack.education.projetolabpcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLabPcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoLabPcpApplication.class, args);
+    }
+
+}
---
+++ projeto-labPCP/src/test/java/br/fullstack/education/projetolabpcp/ProjetoLabPcpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fullstack.education.projetolabpcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLabPcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
